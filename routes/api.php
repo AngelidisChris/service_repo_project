50a62422053bab75e2b16a9172db1985708157ad
Angelidis Christos <angelidischris@gmail.com>
2021-03-27 12:34:51 +0200

    first route creation for endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\VesselController;
+use App\Http\Controllers\VoyageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/voyages', VoyageController::class)->only('index', 'store', 'update');
+
+Route::post('/vessels/{id}/vessel-opex',[ VesselController::class, 'vessel_opex'])->name('vessel.opex');
+
+Route::get('/vessels/{id}/financial-report', [VesselController::class, 'financial_report'])->name('vessel.fin_report');
